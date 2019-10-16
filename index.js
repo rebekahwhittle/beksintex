502fdf80f2b045494f255b4a9d53bd7a13d30f57
rebekahwhittle <whittlebekah@gmail.com>
2019-10-15 20:44:02 -0500

    🕸  treat me to a trick 🎃

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
+$('#open-card').on('click', function() {
+	console.log("🎃");
 
-    // Custom jQuery for tab area
+	$('.inside-card-container').toggleClass("visible");
+	$('.card-container, .peak-inside').addClass("hidden");
+});
 
-    import $ from jquery;
+$('#close-card').on('click', function() {
+	console.log("🦇");
 
-    $(document).ready(function () {
-        //your code here
-        $('.nav').on('click', function(e) {
-            e.preventDefault();
-            console.log("hello world");
-        });
-    });
-
-
-
-	// tabThrough: function() {
-	// 	$('.header-tabs').on('click', function(e){
-	// 		e.preventDefault();
-	// 		var $currentAttrValue = $(this).attr('href');
-    //         var $currentClassToggle = $(this).data('class-toggle');
-    //         var $background = $('#background');
-    //         $('.tab-content .tab').removeClass('active').hide();
-    //         $('.tab-content ' + $currentAttrValue).addClass('active').show();
-    //         $(this).addClass('active');
-    //         $('.tab-links .header-tabs').not(this).removeClass('active');
-    //         $background.removeClass().addClass($currentClassToggle);
-	// 	});
-	// }
-
-	// Custom jQuery for smooth scroll on pricing page
-	// transactionFee: function() {
-	// 	$('#transaction-fee-btn').on('click', function(e) {
-	// 		e.preventDefault();
-	// 		var $root = $('html, body');
-	// 		var $feeModule = $(this).attr('href');
-
-	// 		$root.animate({
-	// 			scrollTop: $( $feeModule ).offset().top-100
-	// 		}, 700);
-	// 	});
-	// },
+	$('.inside-card-container').removeClass("visible");
+	$('.card-container, .peak-inside').removeClass("hidden");
+});
